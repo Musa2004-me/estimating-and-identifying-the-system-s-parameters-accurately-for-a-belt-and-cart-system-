5ca164b23070d11e8d05ac197d00579c4bcba313
Musa2004-me <musaelshenawy5@gmail.com>
2026-05-19 05:15:29 +0300

    Add files via upload

---
+++ CAD/Assem1_STEP_DataFile.m
@@ -0,0 +1,262 @@
+% Simscape(TM) Multibody(TM) version: 7.7
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(20).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(20).angle = 0.0;
+smiData.RigidTransform(20).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(20).ID = "";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [42.416812337983998 75.718811702680995 237.500680301377];  % mm
+smiData.RigidTransform(1).angle = 1.4971596251225678;  % rad
+smiData.RigidTransform(1).axis = [-0.0062396363220655476 -0.99996462787058149 -0.005640030692987027];
+smiData.RigidTransform(1).ID = "RootGround[side.STEP-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [24.310088483881 39.010911832822003 64.249732660406991];  % mm
+smiData.RigidTransform(2).angle = 3.0508814060158529;  % rad
+smiData.RigidTransform(2).axis = [0.70232165766331145 -0.71042338544505002 -0.045198479950681707];
+smiData.RigidTransform(2).ID = "RootGround[Mini Idler Pulley - POM V-Slot.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [30.107518185729003 67.232216922223003 -213.288870529967];  % mm
+smiData.RigidTransform(3).angle = 3.0772323870049116;  % rad
+smiData.RigidTransform(3).axis = [-0.018817039669882268 -0.93046341857733561 -0.36590127863597305];
+smiData.RigidTransform(3).ID = "RootGround[GT2_20T_????????????1.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [44.567239563676999 67.066226214482995 -214.356065337476];  % mm
+smiData.RigidTransform(4).angle = 2.0602879606637643;  % rad
+smiData.RigidTransform(4).axis = [0.51879471009392275 0.55859050567678115 -0.6471697580591359];
+smiData.RigidTransform(4).ID = "RootGround[Shaft Coupler.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [55.656515016203997 76.578549602259997 61.930301463326003];  % mm
+smiData.RigidTransform(5).angle = 1.6632650899694206;  % rad
+smiData.RigidTransform(5).axis = [0.27219590223506884 -0.27623816383480998 -0.92173850285621117];
+smiData.RigidTransform(5).ID = "RootGround[bolt.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [55.223975567761997 38.656035147304998 61.968143761592003];  % mm
+smiData.RigidTransform(6).angle = 3.0508814060158529;  % rad
+smiData.RigidTransform(6).axis = [0.70232165766331145 -0.71042338544505002 -0.045198479950681707];
+smiData.RigidTransform(6).ID = "RootGround[Mini Idler Pulley - POM V-Slot.STEP-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [56.985833881022998 67.383942135982011 245.97851686526602];  % mm
+smiData.RigidTransform(7).angle = 2.5725298281722995;  % rad
+smiData.RigidTransform(7).axis = [-0.65806530775103378 -0.26414185921170419 0.70510930283660367];
+smiData.RigidTransform(7).ID = "RootGround[GT2 idler pulley (5 mm bore) v1.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [17.322632450095 33.794801990196 -231.856788286843];  % mm
+smiData.RigidTransform(8).angle = 0.074555881218479531;  % rad
+smiData.RigidTransform(8).axis = [-0.0077426682023607534 0.98813363128812248 -0.15340136181421957];
+smiData.RigidTransform(8).ID = "RootGround[??????? V-slot 20x20_500.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [21.620053777056 76.92682386421599 21.904285407131002];  % mm
+smiData.RigidTransform(9).angle = 1.6632650899694206;  % rad
+smiData.RigidTransform(9).axis = [0.27219590223506884 -0.27623816383480998 -0.92173850285621117];
+smiData.RigidTransform(9).ID = "RootGround[bolt.STEP-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [8.5579414947080004 75.647219934036997 -221.25150960057499];  % mm
+smiData.RigidTransform(10).angle = 1.4971596251225678;  % rad
+smiData.RigidTransform(10).axis = [-0.0062396363220655424 -0.99996462787058149 -0.0056400306929870218];
+smiData.RigidTransform(10).ID = "RootGround[side.STEP-2]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [52.533940860935999 76.571947178702004 19.622696508316];  % mm
+smiData.RigidTransform(11).angle = 1.6632650899694206;  % rad
+smiData.RigidTransform(11).axis = [0.27219590223506884 -0.27623816383480998 -0.92173850285621117];
+smiData.RigidTransform(11).ID = "RootGround[bolt.STEP-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [24.742627932323998 76.933426287773003 64.211890362140011];  % mm
+smiData.RigidTransform(12).angle = 1.6632650899694206;  % rad
+smiData.RigidTransform(12).axis = [0.27219590223506884 -0.27623816383480998 -0.92173850285621117];
+smiData.RigidTransform(12).ID = "RootGround[bolt.STEP-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(13).translation = [35.121737935407005 67.634931533719993 247.59218901386402];  % mm
+smiData.RigidTransform(13).angle = 1.099654528980853;  % rad
+smiData.RigidTransform(13).axis = [-0.99745339882085671 0.065846002006612905 0.027404766017357987];
+smiData.RigidTransform(13).ID = "RootGround[bolt.STEP-5]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(14).translation = [1.2628670921310001 67.563339765076009 -211.160000888089];  % mm
+smiData.RigidTransform(14).angle = 2.8870670108990666;  % rad
+smiData.RigidTransform(14).axis = [-0.9992939218249538 0.010442977424931518 0.036091578327992271];
+smiData.RigidTransform(14).ID = "RootGround[bolt.STEP-6]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(15).translation = [425.21561939726701 64.370834641268004 -5.2625227233969998];  % mm
+smiData.RigidTransform(15).angle = 2.1312590818772637;  % rad
+smiData.RigidTransform(15).axis = [-0.54682688654931655 0.59544364052147214 0.5885806886989382];
+smiData.RigidTransform(15).ID = "RootGround[base.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(16).translation = [60.522794187614998 66.883064054217996 -215.53365960783199];  % mm
+smiData.RigidTransform(16).angle = 1.756119854608402;  % rad
+smiData.RigidTransform(16).axis = [0.46119738856871539 -0.40652246536738057 0.78869287680885292];
+smiData.RigidTransform(16).ID = "RootGround[JGA25-370 12V Planetary Geared Motor_Ratio 171 & 226.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(17).translation = [52.001036293277004 29.850009461956002 19.669319616724998];  % mm
+smiData.RigidTransform(17).angle = 1.5634685399304695;  % rad
+smiData.RigidTransform(17).axis = [0.064089816250178552 0.064356776743414945 0.99586680873509215];
+smiData.RigidTransform(17).ID = "RootGround[Mini Idler Pulley - POM V-Slot.STEP-3]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(18).translation = [68.344588601883004 75.421173192249995 235.587089612049];  % mm
+smiData.RigidTransform(18).angle = 1.4971596251225678;  % rad
+smiData.RigidTransform(18).axis = [-0.0062396363220650107 -0.99996462787058149 -0.0056400306929865291];
+smiData.RigidTransform(18).ID = "RootGround[side.STEP-1]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(19).translation = [21.087149209396998 30.204886147469001 21.950908515542];  % mm
+smiData.RigidTransform(19).angle = 1.5634685399304695;  % rad
+smiData.RigidTransform(19).axis = [0.064089816250178552 0.064356776743414945 0.99586680873509215];
+smiData.RigidTransform(19).ID = "RootGround[Mini Idler Pulley - POM V-Slot.STEP-4]";
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(20).translation = [40.752470677546995 82.752371738828998 40.318165340088001];  % mm
+smiData.RigidTransform(20).angle = 1.5061632583675526;  % rad
+smiData.RigidTransform(20).axis = [-0.10254450991229776 -0.98987414918974459 0.098151883592348887];
+smiData.RigidTransform(20).ID = "RootGround[GT2 idler pulley (5 mm bore) v1.STEP-2]";
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(9).mass = 0.0;
+smiData.Solid(9).CoM = [0.0 0.0 0.0];
+smiData.Solid(9).MoI = [0.0 0.0 0.0];
+smiData.Solid(9).PoI = [0.0 0.0 0.0];
+smiData.Solid(9).color = [0.0 0.0 0.0];
+smiData.Solid(9).opacity = 0.0;
+smiData.Solid(9).ID = "";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.016002738853356169;  % kg
+smiData.Solid(1).CoM = [10.055121623041737 -17.048170122669216 3];  % mm
+smiData.Solid(1).MoI = [6.4664431780798477 2.1916837836046068 8.5621105285643182];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0.0054794114451213573];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = "side.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.0012766653456037511;  % kg
+smiData.Solid(2).CoM = [4.399999939034835 0 0];  % mm
+smiData.Solid(2).MoI = [0.040530682862276776 0.027796964550667089 0.027796964550667078];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(2).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = "Mini Idler Pulley - POM V-Slot.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.0021500077367758133;  % kg
+smiData.Solid(3).CoM = [7.1216812582548039 -0.042400792964445211 -0.042402533072736857];  % mm
+smiData.Solid(3).MoI = [0.066028753145291386 0.082841274890630701 0.082841282791338594];  % kg*mm^2
+smiData.Solid(3).PoI = [3.8613064868494586e-06 -0.0002846614903404885 -0.00028462058596283898];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = "GT2_20T_????????????1.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.0034648744657145812;  % kg
+smiData.Solid(4).CoM = [0.10997416727443231 0.2075260899105669 0.11003778731191818];  % mm
+smiData.Solid(4).MoI = [0.21552597206663357 0.11777433612984847 0.21537918521341698];  % kg*mm^2
+smiData.Solid(4).PoI = [-0.00043459368070291379 2.1777770483867735e-05 -6.9523391917625894e-05];  % kg*mm^2
+smiData.Solid(4).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = "Shaft Coupler.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 0.00095213636764532891;  % kg
+smiData.Solid(5).CoM = [19.405408644848162 -0.0011315270980941608 0.0013875442318816594];  % mm
+smiData.Solid(5).MoI = [0.0034882616920120144 0.23208875802119769 0.23209167712960913];  % kg*mm^2
+smiData.Solid(5).PoI = [-2.1730857184371653e-07 -7.0386927934835326e-06 -2.6700848636073203e-07];  % kg*mm^2
+smiData.Solid(5).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = "bolt.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(6).mass = 0.00094266000845084636;  % kg
+smiData.Solid(6).CoM = [0 0 3.25];  % mm
+smiData.Solid(6).MoI = [0.019004456586132621 0.019004456586132635 0.024376050148878857];  % kg*mm^2
+smiData.Solid(6).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(6).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(6).opacity = 1;
+smiData.Solid(6).ID = "GT2 idler pulley (5 mm bore) v1.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(7).mass = 0.084855334164726598;  % kg
+smiData.Solid(7).CoM = [0 0 249.99999999999997];  % mm
+smiData.Solid(7).MoI = [1771.2370262865984 1771.2370262865984 6.835129042922345];  % kg*mm^2
+smiData.Solid(7).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(7).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(7).opacity = 1;
+smiData.Solid(7).ID = "??????? V-slot 20x20_500.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(8).mass = 0.028548000000000007;  % kg
+smiData.Solid(8).CoM = [-16.435450513898932 389.11764705882359 3];  % mm
+smiData.Solid(8).MoI = [11.827723795315 11.493165130365618 23.149600925680616];  % kg*mm^2
+smiData.Solid(8).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(8).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(8).opacity = 1;
+smiData.Solid(8).ID = "base.STEP*:*Default";
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(9).mass = 0.010900766259702746;  % kg
+smiData.Solid(9).CoM = [4.5293063581255417e-06 -18.324472794656913 0.0052090639659596195];  % mm
+smiData.Solid(9).MoI = [3.2174737833092308 1.0647577825152221 3.2178597253814964];  % kg*mm^2
+smiData.Solid(9).PoI = [0.0011829596383240099 -7.6765657113300322e-09 1.5387461053286444e-06];  % kg*mm^2
+smiData.Solid(9).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(9).opacity = 1;
+smiData.Solid(9).ID = "JGA25-370 12V Planetary Geared Motor_Ratio 171 & 226.STEP*:*Default";
+
